d4a499de6f1f11f2e8dfcf2e865d4a729724fdd8
Boaz Blake <boazblake@gmail.com>
2024-09-01 08:18:09 -0500

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 import legacy from "@vitejs/plugin-legacy";
 import path from "path";
 
@@ -8,6 +9,12 @@ export default defineConfig({
   base: "/ion-bgo/",
   plugins: [
     vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+    }),
     legacy({
       targets: ["ie >= 11"],
       additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
